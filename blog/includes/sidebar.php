b26244130bc96f80a35d033dca2981b0c052ae1a
karineWJ <kwujye@gmail.com>
2014-12-05 12:01:34 -0800

    Added sidebar dynamics

---
+++ blog/includes/sidebar.php
@@ -0,0 +1,64 @@
+<aside id="sidebar" class="cf">
+	<?php //get the title and post_id of the latest 5 published posts
+	$query_latest = "SELECT title, post_id
+					 FROM posts
+					 WHERE is_published = 1
+					 ORDER BY date DESC
+					 LIMIT 5";
+	//run it
+	$result_latest = $db->query($query_latest);
+	//check to see if any posts were found
+	if( $result_latest->num_rows >= 1 ){
+	?>
+	<section>
+		<h2>Latest Posts</h2>
+		<ul>
+			<?php while( $row_latest = $result_latest->fetch_assoc() ){ ?>
+				<li><a href="#"><?php echo $row_latest['title'] ?></a></li>
+			<?php }//end while ?>
+		</ul>
+	</section>
+	<?php } //end if posts found ?>
+
+
+
+	<?php //get the titles of all categories in alphabetic order
+	$query_cats = "SELECT title
+				   FROM categories
+				   ORDER BY title ASC"; //if use ORDER BY RAND()==> it will randomize
+	//run it
+	$result_cats = $db->query($query_cats);
+	//check to see if any posts were found
+	if ( $result_cats->num_rows >=1 ){
+	?>
+	<section>
+		<h2>Categories</h2>
+		<ul>
+			<?php while( $row_cats = $result_cats->fetch_assoc() ){ ?>
+				<li><a href="#"><?php echo $row_cats['title'] ?></a></li>
+			<?php } ?>
+		</ul>
+	</section>
+	<?php } //end if posts found ?>
+
+	
+
+	<?php //get the title of all links
+	$query_links ="SELECT title
+				   FROM links"; 
+	//run it
+	$result_links = $db->query($query_links);
+	//check to see if any posts were found
+	if( $result_links->num_rows >=1 ){			   
+	?>
+	<section>
+		<h2>Links</h2>
+		<ul>
+			<?php while( $row_links = $result_links->fetch_assoc() ){ ?>
+				<li><a href="#"><?php echo $row_links['title'] ?></a></li>
+			<?php } ?>
+		</ul>
+	</section>
+	<?php } //end if posts found ?>
+
+</aside>
--- blog/index.php
+++ blog/index.php
@@ -11,6 +11,7 @@ require_once('includes/functions.php');
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Average+Sans' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<header id="banner">
@@ -19,16 +20,25 @@ require_once('includes/functions.php');
 	
 	<main id="content">
 		<?php //get all the published posts, most recent first
-			$query = "SELECT * FROM posts WHERE is_published = 1 ORDER BY date DESC"; //DESC=>newest to oldest post; ASC=>oldest to newest
+			$query = "SELECT posts.*, users.username , categories.title AS category
+					  FROM posts, users, post_cats, categories
+					  WHERE is_published = 1 
+					  AND posts.user_id = users.user_id
+					  AND posts.post_id = post_cats.post_id
+					  AND categories.category_id = post_cats.category_id
+					  ORDER BY date DESC "; //DESC=>newest to oldest post; ASC=>oldest to newest
 			//Run the query. Hold onto the results in a variable
 			$result = $db->query( $query );
 			//check to see if one or more rows were found
 			if( $result->num_rows >= 1 ){
-				while( $row = $result->fetch_assoc() ){
+				while( $row = $result->fetch_assoc() ){ //fetch_assoc gets the next post
 			?>
 			<article class="post">
 				<h1><?php echo $row['title'] ?></h1>
-				<div class="date"><?php echo convert_date( $row['date'] ) //'convert_date function' will clean the date format ?></div>
+				<div class="date">By <?php echo $row['username'] ?>
+					<?php echo convert_date( $row['date'] ) //'convert_date function' will clean the date format ?>
+					In the category <?php echo $row['category'] ?>
+				</div>
 				<p><?php echo $row['body'] ?></p>
 			</article>
 
@@ -41,8 +51,8 @@ require_once('includes/functions.php');
 		?>
 	</main>
 	
-	<aside id="sidebar"></aside>
-	
-	<footer id="colophon">&copy; 2014 Karine Wu Jye</footer>
+	<?php include('includes/sidebar.php'); ?>
+
+	<footer id="colophon" class="cf">&copy; 2014 Karine Wu Jye</footer>
 </body>
 </html>
